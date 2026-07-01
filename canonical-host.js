84e34d29f3121a2f08ef657d6afdaa74ad04bffe
Phil <agent.phil@icloud.com>
2026-07-01 09:12:56 -0500

    chore: remove legacy root static duplicates

--- canonical-host.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  var host = window.location.hostname.toLowerCase();
-  if (host === 'protect30a.com' || host === 'www.protect30a.com' || host === 'www.protect30a.org') {
-    window.location.replace('https://protect30a.org' + window.location.pathname + window.location.search + window.location.hash);
-  }
-}());
